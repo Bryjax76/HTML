165742c0559540cb69fb40be51fe63848458a8ec
Bryjax76 <66446836+Bryjax76@users.noreply.github.com>
2021-06-07 20:18:05 +0200

    Add files via upload

---
+++ Excercise/ES10/script.js
@@ -0,0 +1,28 @@
+/*const array = [1,2,[3,4,[5]]]
+array.flat(2);
+const array2 = [1,2,3,[4,5,6]]
+array2.flat(2);
+
+
+const entries = ['Bob',,,,,,,, 'Sally',]
+entries.flat()
+
+const array3 = array.flatMap(num => num + 5)
+
+const userEmail = '            gfsiogn@gmail.com';
+const userEmail2 = 'adifnod@gmail.com            ';
+console.log(userEmail.trimStart())
+console.log(userEmail2.trimEnd())
+*/
+
+userProfiles = [['commanderTom', 23], ['tomShelby', 40], ['Jerry', 'Teen']]
+Object.fromEntries(userProfiles)
+
+
+try{
+    asoi + 'hi'
+}
+catch (error)
+{
+    console.log('You messed up.... ' + error);
+}
