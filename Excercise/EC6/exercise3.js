02d35bac1c64cfcc212337bf9f7376d3c7009412
Bryjax76 <66446836+Bryjax76@users.noreply.github.com>
2021-06-07 11:15:54 +0200

    Update exercise3.js

--- Excercise/EC6/exercise3.js
+++ Excercise/EC6/exercise3.js
@@ -59,4 +59,4 @@ function whereAmI(username, location) {
         return "I am totally lost!";
     }
 }
-const whereAmI(username, location) => if(username&&location){return"I am not lost"};
+const whereAmI = (username, location) =>{if (username&&location) {return"I am not lost"} else {return "I am totally lost!"}};
